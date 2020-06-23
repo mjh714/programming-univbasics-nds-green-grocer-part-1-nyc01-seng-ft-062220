b9625a49346a618ae7ef03e8efafdfd1d0c3d7b8
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-23 12:04:14 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,9 +11,11 @@ def consolidate_cart(cart)
   new_cart = {} 
   cart.each do |items_array| 
     items_array.each do |item, attribute_hash| 
-      new_cart[item] ||= attribute_hash 
-      new_cart[item][:count] ? new_cart[item][:count] += 1   
-      new_cart[item][:count] = 1 
+     
+    if new_cart.include? (attribute_hash) new_cart[item][:count] += 1 
+    else
+      new_cart[item][:count] = 1
+    end
   end 
 end 
 new_cart 
